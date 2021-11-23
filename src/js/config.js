29f871fe51ee4722d82d95f8176131d3c780a835
unknown <bora.karaca@ogr.iu.edu.tr>
2021-11-23 09:35:40 +0300

    Inital commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 11;
+export const KEY = '23d19768-ff2c-4066-882c-0decd6cd39c4';
+export const MODAL_CLOSE_SEC = 2.5;
